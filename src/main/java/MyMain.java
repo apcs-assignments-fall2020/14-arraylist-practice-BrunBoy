1ad634b8aa22bdf99f95739b3daf4d1f0dcf85ab
BrunBoy <23rreddy@athenian.org>
2020-11-06 03:08:45 +0000

    SQUEAKMOUSEMAYROCKDONE

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,20 +4,39 @@ public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int count=0;
+        for (int i=0; i<list.size(); i++){
+            Integer drrBrr=list.get(i);
+            if (!(drrBrr%2==0)){
+                count++;
+            }
+        }
+        return count;
     }
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
+        for (int i=0; i<list1.size(); i++){
+            Integer skraa=list1.get(i);
+            for (int j=0; j<list2.size(); j++){
+                Integer meerkat=list2.get(j);
+                if (skraa==meerkat){
+                    return true;
+                }
+            }
+
+        }
         return false;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
-    public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
+    public static ArrayList<Integer> convertToArrayList(int[] arr) {
+        //looked at 2 examples to understand how to make a new ArrayList
+        ArrayList<Integer> ohSleuthyOne= new ArrayList<Integer>();
+        for (int i=0; i<arr.length; i++){
+            ohSleuthyOne.add(arr[i]);
+        }
+        return ohSleuthyOne;
     }
 
 
